daaa6d3bcca4b25a126eabad194372b455ab19cd
mp222sf <mp222sf@student.lnu.se>
2015-10-29 08:43:32 +0100

    Update Settings.php

--- Settings.php
+++ Settings.php
@@ -6,8 +6,8 @@
 class Settings {
 
 	// Settings for setup of the MYSQL-connection.
-	const MYSQL_SERVER = "127.0.0.1";
-	const MYSQL_USERNAME = "root";
+	const MYSQL_SERVER = "";
+	const MYSQL_USERNAME = "";
 	const MYSQL_PASSWORD = "";
 	const MYSQL_DATABASE = "highscores";
 	const MYSQL_PORT = "3306";
